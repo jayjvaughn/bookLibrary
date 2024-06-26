082fb05bb71c9392af55c7d5fe1865f15668747a
jayjvaughn <waveyjxyshop@gmail.com>
2024-06-26 02:50:56 -0400

    created my basic add new book popup box

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function book(title, author, pageCount, isRead){
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.isRead = isRead;
+};
+
+function addBookToLibrary(){
+
+};
