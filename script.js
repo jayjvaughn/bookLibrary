4ac594e02b6f9e514f7f5075991c2ab7d5b98a18
jayjvaughn <waveyjxyshop@gmail.com>
2024-07-06 18:05:32 -0400

    so many changes, read below
    I redesigned my overall library design, I then added the functionality of the isBookRead radio input, however im going to rewrite it because it works but not as i intend for it to work, its very overcomplicated. I also made it so that once you add your book to your library it also adds it to an index to save the books information

--- script.js
+++ script.js
@@ -1,12 +1,82 @@
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const isRead = document.querySelector("#yes");
+const isNotRead = document.querySelector("#no");
+const popUpBtn = document.querySelector(".addBookPopUp");
+const addBtn = document.querySelector(".submitButton");
+const containerBody = document.querySelector("#containerBody")
+
+
 const myLibrary = [];
 
 function book(title, author, pageCount, isRead){
     this.title = title;
     this.author = author;
-    this.pageCount = pageCount;
     this.isRead = isRead;
 };
 
 function addBookToLibrary(){
+    myLibrary.push(book);
+};
+
+addBtn.addEventListener("click", bookCard);
+
+                    // Book Cards
+function bookCard(){
+const card = document.createElement("div");
+    card.classList.add('card');
+    containerBody.append(card);
+
+const bookTitle = document.createElement("div");
+    bookTitle.textContent = title.value;
+    bookTitle.classList.add("cardTitle");
+    card.appendChild(bookTitle);
+
+const bookAuthor =  document.createElement("div");
+bookAuthor.textContent = "Author: " + author.value;
+card.appendChild(bookAuthor);
+
+const isBookRead = function isBookReadChecker(){
+    if(yes.checked){
+    const isBookRead = document.createElement("div");
+    isBookRead.textContent = "Status: " + "Finsihed";
+    card.appendChild(isBookRead);
+    }
+    else if(no.checked){
+        const isBookRead = document.createElement("div");
+        isBookRead.textContent = "Status: " + "Still reading...";
+        card.appendChild(isBookRead);
+    }
+
+    const newBook = new book(title.value, author.value, );
+    console.log(newBook);
+    myLibrary.push(newBook);
+
+};
+isBookRead();
+                
+                // Card Buttons
+const btnDiv = document.createElement("div");
+        btnDiv.classList.add('btnDiv');
+        card.appendChild(btnDiv);
+const readBtn = document.createElement("button");
+        readBtn.classList.add('readBtn');
+        readBtn.textContent = "Status";
+        btnDiv.appendChild(readBtn);
+const deleteBookBtn = document.createElement("button");
+    deleteBookBtn.classList.add('deleteBookBtn');
+    deleteBookBtn.textContent = "Delete";
+    btnDiv.appendChild(deleteBookBtn);
+
+    readBtn.addEventListener("click", ()=>{
+        if(isBookRead.textContent = "Status: " + "Finsihed"){
+            isBookRead.textContent = "Status: " + "Still reading...";
+        }
+        else if(isBookRead.textContent = "Status: " + "Still reading..."){
+            isBookRead.textContent = "Status: " + "Finsihed";
+        }
+    });
+
+containerBody.appendChild(card);
 
 };
